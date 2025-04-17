d7636a7a10c31bdeef6098e8870c04e42a10c76f
Jykata <jykatabg@gmail.com>
2025-04-17 13:15:46 +0300

    Add: arcjet protection

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ import userRouter from './routes/user.routes.js';
 import connectToDatabase from './database/db.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser'
+import arjcetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(arjcetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/recipes', recipeRouter);
