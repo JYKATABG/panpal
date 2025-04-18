46d40101e801b2a1d214bc8a9fba9a570cdc009a
Jykata <jykatabg@gmail.com>
2025-04-18 13:42:28 +0300

    Fix: check token in authorization middleware

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -4,11 +4,10 @@ import User from "../models/user.model.js";
 
 const authorize = async (req, res, next) => {
     try {
-        let token;
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(' ')[1];
-        }
-
+        const token =
+            req.cookies.token ||
+            (req.headers.authorization.startsWith('Bearer')) && req.headers.authorization.split(" ")[1];
+            
         if (!token) {
             return res.status(401).json({ success: false, message: "Unauthorized" });
         }
