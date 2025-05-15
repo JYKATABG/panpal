250fa8dda98e548a5c6bfef55883aa7b32046f44
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2025-05-15 22:49:33 +0300

    vite config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   preview: {
     port: parseInt(process.env.PORT) || 4173,
     host: "0.0.0.0",
+    allowedHosts: ["calm-analysis-production.up.railway.app", "localhost"],
   },
 });
